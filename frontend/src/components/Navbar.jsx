df5e4e9130ce98487c49fc0aefcee299787a488c
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-11 12:05:33 +0530

    create navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,28 +4,28 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-        <img src={assets.logo} alt="" />
-        <ul>
-            <NavLink>
+    <div className = "flex item-center justify-between text-sm py-4 mb-5">
+        <img className = "w-44 cursor-pointer"  src={assets.logo} alt="" />
+        <ul className ="flex sm:justify-center space-x-4">
+            <NavLink className="no-underline">
                 <li>HOME</li>
                 <hr />
             </NavLink>
-            <NavLink>
+            <NavLink className="no-underline">
                 <li>ALL DOCTORS</li>
                 <hr />
             </NavLink>
-            <NavLink>
+            <NavLink className="no-underline">
                 <li>ABOUT</li>
                 <hr />
             </NavLink>
-            <NavLink>
+            <NavLink className="no-underline">
                 <li>CONTACT</li>
                 <hr />
             </NavLink>
         </ul>
         <div>
-            <button>Create Account</button>
+            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors duration-200">Create Account</button>
         </div>
     </div>
   )
