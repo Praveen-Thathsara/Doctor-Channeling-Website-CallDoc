e46de5e059823646889111e8cf58995c639f5078
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-16 11:56:04 +0530

    create TopDoctors section and create navigate functions using appContext.jsx and modify tailwins configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colours :{
+        'primary': "#00A4A6",
+        'background':"#00A4A640"
+      },
+      gridTemplateColumns:{
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))'
+      }
+    },
   },
   plugins: [],
 }
