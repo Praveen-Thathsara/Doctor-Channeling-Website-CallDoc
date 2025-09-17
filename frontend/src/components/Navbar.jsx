500c4e9f0b182c72af2e436cf144735d499aac56
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-17 11:57:32 +0530

    set up the doctors page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5">
-      <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
+      <img onClick={()=>navigate('/')} className="w-44 cursor-pointer" src={assets.logo} alt="" />
 
       {/* Corrected UL structure */}
       <ul className="flex sm:justify-center space-x-6">
