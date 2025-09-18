7319ad3eafe976c5d5090ce0e634411373757cd4
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-18 10:00:14 +0530

    create doc details section

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -7,43 +7,45 @@ import { assets } from '../assets/assets'
 const Appointment = () => {
 
   const { docId } = useParams()
-  const {doctors} = useContext(AppContext)
-  const [docInfo,setDocInfo] = useState(null)
-  const fetchDocinfo = async ()=>{
+  const { doctors } = useContext(AppContext)
+  const [docInfo, setDocInfo] = useState(null)
+  const fetchDocinfo = async () => {
     const docInfo = doctors.find(doc => doc._id === docId)
     setDocInfo(docInfo)
     console.log(docInfo);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchDocinfo()
 
-  },[doctors, docId])
+  }, [doctors, docId])
 
   return docInfo && (
     <div>
+    <div className='flex flex-col sm:flex-row gap-4'>
       {/*doctor details*/}
       <div >
-        <div><img src={docInfo.image} alt="" /></div>
+        <div><img className='bg-[#00A4A6] w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" /></div>
       </div>
 
-      <div>
+      <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
         {/*doc info name degree experience*/}
-        <p>{docInfo.name}
-           <img src={assets.verified_icon} alt="" />
-           </p>
-           <div>
-            <p>{docInfo.degree} - {docInfo.speciality}</p>
-            <button>{docInfo.experience}</button>
-           </div>
+        <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>{docInfo.name}
+          <img className='w-5' src={assets.verified_icon} alt="" />
+        </p>
+        <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+          <p>{docInfo.degree} - {docInfo.speciality}</p>
+          <button className='py-0.5 px-2 border text-xs rounded-full'>{docInfo.experience}</button>
+        </div>
 
-           {/*doctors about*/}
-           <div>
-            <p>About <img src={assets.info_icon} alt="" /></p>
-            <p>{docInfo.about}</p>
-           </div>
+        {/*doctors about*/}
+        <div>
+          <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>About <img src={assets.info_icon} alt="" /></p>
+          <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
+        </div>
 
       </div>
+    </div>
     </div>
   )
 }
