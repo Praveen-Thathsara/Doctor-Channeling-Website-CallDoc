a6d961427ff845e33739a79c79353ec97634631f
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-18 10:52:19 +0530

    setting the date slots

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
@@ -9,16 +9,45 @@ const Appointment = () => {
   const { docId } = useParams()
   const { doctors, currencySymbol } = useContext(AppContext)
   const [docInfo, setDocInfo] = useState(null)
+  const [docSlots, setDocSlots] = useState([])
+  const [slotIndex, setSlotIndex] = useState(0) 
+  const [slotTime, setSlotTime] = useState('')
   const fetchDocinfo = async () => {
     const docInfo = doctors.find(doc => doc._id === docId)
     setDocInfo(docInfo)
     console.log(docInfo);
   }
 
+  const getAvailableSlots = () => {
+    setDocSlots([])
+
+  }
+
+  //getting current date
+  let today = new Date()
+
+  for(let i=0; i<7; i++){
+    //getting date with index
+    let currentDate = new Date(today)
+    currentDate.setDate(today.getDate() + i)
+
+    //setting end time of the date with index
+    let endTime = new Date()
+    endTime.setHours(today.getDate()+1)
+    endTime.sethours(21,0,0,0)
+
+    //setting hours
+    if(today.getDate() === currentDate.getDate()){
+  }
+
   useEffect(() => {
     fetchDocinfo()
 
   }, [doctors, docId])
+
+  useEffect(() => {
+    getAvailableSlots
+  },[docInfo])
 
   return docInfo && (
     <div>
@@ -43,14 +72,15 @@ const Appointment = () => {
           <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>About <img src={assets.info_icon} alt="" /></p>
           <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
         </div>
-        <p>
-          Appointment fee: <span>{currencySymbol}{docInfo.fees}</span>
+        <p className='text-gray-500 font-medium mt-4'>
+          Appointment fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span>
         </p>
 
       </div>
     </div>
     </div>
   )
+}
 }
 
 export default Appointment
