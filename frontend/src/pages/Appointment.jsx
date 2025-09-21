c0feee86dc62d7098e7c34c50779f679791a603b
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-21 13:34:19 +0530

    complete the date time appointment section

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -126,11 +126,12 @@ const Appointment = () => {
         </div>
         <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
           {docSlots.length && docSlots[slotIndex].map((item, index)=>(
-            <p className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer`} key={index}>
+            <p onClick={()=> setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-[#00A4A6] text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
               {item.time.toLowerCase()}
             </p>
             ))}
         </div>
+        <button className='bg-[#00A4A6] text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book on Appointment</button>
 
       </div>
     </div>
