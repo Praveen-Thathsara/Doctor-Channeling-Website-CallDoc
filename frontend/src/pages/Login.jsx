5abc3bb36ccaba82ec93444e6dd3280561d8c194
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-22 21:46:24 +0530

    create login and sign up pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+
+
+  const [state, setState] = useState('Sign Up')
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+  const onSubmitHandler  = async (event) => {
+    event.preventDefault()
+  }
+
   return (
-    <div>
+    <form className='min-h-[80vh] flex items-center'>
+      <div className='flex flex-col gap-3 m-auto item-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+        <p className='text-2xl font-semibold'>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
+        <p>Please {state === 'Sign Up' ? "Sign Up" : "log in"} to book appointment</p>
+        <div className='w-full'>
+          <p>Full Name</p>
+        <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" onChange={(e)=>setName(e.target.name)} value={name} />
+        </div>
+        <div className='w-full'>
+          <p>Email</p>
+        <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="email" onChange={(e)=>setEmail(e.target.name)} value={email} />
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+        <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" onChange={(e)=>setPassword(e.target.name)} value={password} />
+        </div>
+        <button className='bg-[#00A4A6] text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+        {
+          state === 'Sign Up'
+          ? <p>Already have an account? <span onClick={()=>setState('Login')} className='text-[#00A4A6] underline cursor-pointer'>Login here</span></p> 
+          : <p>Create an new account? <span onClick={()=>setState('Sign Up')} className='text-[#00A4A6] underline cursor-pointer'>click here</span></p>
+        }
         
-    </div>
+      </div>
+    
+    </form>
   )
 }
 
