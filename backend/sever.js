1377395c4a0429a9c207286d4603e01a089ac08a
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-24 11:48:00 +0530

    create backend files and import the frameworks

---
+++ backend/sever.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+
+
+//app config
+const app = express()
+const port = process.env.PORT || 4000
+
+//middlewars
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.get('/', (req, res) => {
+    res.send('api working')
+})
+
+app.listen(port, () => console.log("server started", port))
