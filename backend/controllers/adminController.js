7a4e30d80a22843cd9499ae6968d5bd9bef51f45
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-26 21:15:14 +0530

    check the doctor adding api using postman and console.log in file adminController

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,9 +1,11 @@
-// adding doctor
+//API fo the adding doctor
 
 const addDoctor = async (req, res) => {
     try{
         const{ name, email, password, image, speciality, degree, experience, about, fees, address}  = req.body
+        const imageFile = req.file 
 
+        console.log({ name, email, password, image, speciality, degree, experience, about, fees, address},imageFile);
 
     }catch(error){
 
