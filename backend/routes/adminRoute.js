cb24b21f1780e9407e984e7b4519671191c021d9
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-27 19:34:46 +0530

    admin login API is working successfully and fix some file path error in multer.js

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import { addDoctor, loginAdmin } from '../controllers/adminController.js'
-import upload from '../models/multer.js'
-
+import upload from '../middlewares/multer.js'
 
 const adminRouter = express.Router()
 
