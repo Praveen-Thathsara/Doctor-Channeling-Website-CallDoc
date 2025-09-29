3528a755776f1040a71210ba3130a24a9a17368e
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-29 14:40:51 +0530

    import assets and make some file changes

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AdminContext } from './context/AdminContext.jsx'
+import { DoctorContext } from './context/DoctorContext.jsx'
+import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <AdminContext.Provider>
+      <DoctorContext.Provider>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </DoctorContext.Provider>
+    </AdminContext.Provider>
+  </BrowserRouter>,
 )
---
+++ admin/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Login = () => {
+
+
+    const [state, setState] = useState('Admin')
+
+  return (
+    <form>
+        <div>
+            
+        </div>
+    </form>
+  )
+}
+
+export default Login
