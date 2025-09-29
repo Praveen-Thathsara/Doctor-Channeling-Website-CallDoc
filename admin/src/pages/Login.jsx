5a4bedf6382daea38b3b209f770f1b2309e02f44
Praveen Thathsara <praveenstudy823@gmail.com>
2025-09-29 15:00:46 +0530

    create admin login page

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useState } from 'react'
 
 const Login = () => {
 
@@ -7,9 +8,19 @@ const Login = () => {
     const [state, setState] = useState('Admin')
 
   return (
-    <form>
-        <div>
-            
+    <form className='min-h-[80vh] flex items-center'>
+        <div className='flex flex-col gap-3 m-auto item-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+            <p className='text-2xl font-semibold m-auto'><span className='text-primary'>{state}</span> Login</p>
+            <div className='w-full'>
+                <p>Email</p>
+                <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
+            </div>
+            <div className='w-full'>
+                <p>Password</p>
+                <input className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
+            </div>
+            <button className='bg-primary text-white w-full py-2 rounded-md text-base'>Login</button>
+
         </div>
     </form>
   )
