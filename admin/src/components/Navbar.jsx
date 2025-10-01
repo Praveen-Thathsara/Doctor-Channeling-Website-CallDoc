808a73e27b57797a96037211b753117d7862cee6
Praveen Thathsara <praveenstudy823@gmail.com>
2025-10-02 00:57:32 +0530

    add some css property for the navbar

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -8,12 +8,12 @@ const Navbar = () => {
     const {aToken} = useContext(AdminContext)
 
   return (
-    <div>
-        <div>
-            <img src={assets.admin_logo} alt=''/>
-            <p>{aToken ? 'Admin' : 'Doctor'}</p>
+    <div className='flex justify-between items-center px-4 sm:px-8 py-3 bg-white border-b'>
+        <div className='flex items-center gap-2 text-xs'>
+            <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt=''/>
+            <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
         </div>
-        <button>Logout</button>
+        <button className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
     </div>
   )
 }
