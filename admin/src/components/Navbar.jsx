c6c9b4ae0cbaa05a1ad6f57ead8a5ec67bc400a4
Praveen Thathsara <praveenstudy823@gmail.com>
2025-10-01 11:14:56 +0530

    create the navbar

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useContext } from 'react'
+import { AdminContext } from '../context/AdminContext'
+import {assets} from '../assets/assets'
+
+const Navbar = () => {
+
+    const {aToken} = useContext(AdminContext)
+
+  return (
+    <div>
+        <div>
+            <img src={assets.admin_logo} alt=''/>
+            <p>{aToken ? 'Admin' : 'Doctor'}</p>
+        </div>
+        <button>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
