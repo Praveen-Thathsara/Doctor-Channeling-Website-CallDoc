bf169b1e7642feeaa8797709caef82cf43321793
Praveen Thathsara <praveenstudy823@gmail.com>
2025-10-07 14:31:28 +0530

    create the all doctor page structure

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -16,6 +16,14 @@ const DoctorsList = () => {
                 doctors.map((item,index)=>(
                     <div key={index}>
 <img src={item.image} alt="" />
+<div>
+    <p>{item.name}</p>
+    <p>{item.speciality}</p>
+    <div>
+        <input type="checkbox" checked={item.available} />
+        <p>Available</p>
+    </div>
+</div>
                     </div>
                 ))
             }
