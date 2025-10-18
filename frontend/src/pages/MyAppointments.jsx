550846457dd1ff2b0ff359ad8d704154f8a1c2e5
Praveen Thathsara <praveenstudy823@gmail.com>
2025-10-19 01:02:51 +0530

    create the cancel appointment function , create the API for cancelled the appointment

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -5,6 +5,7 @@ import { assets } from '../assets/assets'
 import { useState } from 'react'
 import axios from 'axios'
 import { useEffect } from 'react'
+import {toast} from 'react-toastify'
 
 const MyAppointments = () => {
 
@@ -12,11 +13,11 @@ const MyAppointments = () => {
   const { backendUrl, token } = useContext(AppContext)
 
   const [appointments, setAppointments] = useState([])
-  const months = ["","Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-  const slotDateFormat = (slotDate)=> {
-const dateArray = slotDate.split('_')
-return dateArray[0]+" "+ months[Number(dateArray[1])] + " " + dateArray[2]
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split('_')
+    return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
   }
 
   const getUserAppointments = async () => {
@@ -26,6 +27,24 @@ return dateArray[0]+" "+ months[Number(dateArray[1])] + " " + dateArray[2]
         setAppointments(data.appointments.reverse())
         console.log(data.appointments)
       }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
+  const cancelAppointment = async (appointmentId) => {
+    try {
+
+      const { data } = await axios.post(backendUrl + '/api/user/cancel-appointment', { appointmentId }, { headers: { token } })
+      if (data.success) {
+        toast.success(data.message)
+        getUserAppointments()
+
+      } else {
+        toast.error(data.message)
+      }
+
     } catch (error) {
       console.log(error)
       toast.error(error.message)
@@ -58,7 +77,7 @@ return dateArray[0]+" "+ months[Number(dateArray[1])] + " " + dateArray[2]
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
               <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-[#00A4A6] hover:text-white transition-all duration-300'>Pay Online</button>
-              <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>
+              <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button>
             </div>
           </div>
         )}
