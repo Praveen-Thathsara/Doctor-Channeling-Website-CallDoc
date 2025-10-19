62677ffad87ddd4b63b61fcd43648fbfed6a425e
Praveen Thathsara <praveenstudy823@gmail.com>
2025-10-20 01:03:06 +0530

    install razorpay and import it to userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken'
 import { v2 as cloudinary } from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import appointmentModel from '../models/appointmentModel.js'
-
+import razorpay from 'razorpay'
 //API to register user
 
 const registerUser = async (req, res) => {
@@ -214,4 +214,13 @@ const cancelAppointment = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+// API to make payment of appointment using razorpay
+
+const paymentRazorpay = async (req, res) => {
+
+}
+
+
+
 export { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment }
