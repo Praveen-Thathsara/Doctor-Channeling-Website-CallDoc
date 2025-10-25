e1bc85f61f01f2b775387b8ea7ae1459cd7e2b4e
Praveen Thathsara <praveenstudy823@gmail.com>
2025-10-25 19:59:45 +0530

    create APIs for get and update the profile data in the doctorpanel

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useContext } from 'react'
+import { DoctorContext } from '../../context/DoctorContext.jsx';
+import { AppContext } from '../../context/AppContext.jsx';
 
 const DoctorProfile = () => {
+
+  const { dToken, profileData,setProfileData, getProfileData } = useContext(DoctorContext);
+  const {currency , backendUrl} = useContext(AppContext);
+
+  useEffect (() => {
+    if (dToken) {
+      getProfileData();
+    }
+  },[dToken])
+
   return (
     <div>DoctorProfile</div>
   )
--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -149,4 +149,44 @@ const doctorDashboard = async (req, res) => {
     }
 }
 
-export { changeAvailability, doctorList, loginDoctor, appointmentsDoctor, appointmentCancel, appointmentComplete, doctorDashboard}
+// API to get doctor profile for Doctor panel
+
+const doctorProfile = async (req, res) => {
+    try {
+
+        const { docId } = req.doctor
+        const profileData = await doctorModel.findById(docId).select('-password')
+
+        res.json({ success: true, profileData })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//API to update doctor profile data from Doctor panel
+
+const updateDoctorProfile = async (req, res) => {
+    try {
+
+        const { docId, fees, address, available } = req.body
+        await doctorModel.findByIdAndUpdate(docId, { fees, address, available })
+
+        res.json({ success: true, message: "Profile Update" })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export { 
+changeAvailability, 
+doctorList, 
+loginDoctor, 
+appointmentsDoctor, 
+appointmentCancel, 
+appointmentComplete, 
+doctorDashboard, 
+doctorProfile, 
+updateDoctorProfile }
